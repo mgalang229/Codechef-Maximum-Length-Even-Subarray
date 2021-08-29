21b6810c8bf7a27774a00bc0acfbc1b7bb830631
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-29 17:47:44 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-#define ll long long
-
-void solve() {
-	int n;
-	cin >> n;
-	//check if the sum of 1 to n is even, if not then, subtract 1 from n
-	cout << ((n*(n+1)/2)%2==0?n:n-1) << "\n";
-}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	int t;
-	cin >> t;
-	while(t--)
-		solve();
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		// use the sum formula to check if the sum of 1 to 'n' is already even
+		cout << ((n * (n + 1)) / 2 % 2 == 0 ? n : n - 1) << '\n';
+	}
+	return 0;
 }
